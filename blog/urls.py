a885c3f70ce3cdadf4f7ce99c0921ed33667aeb4
Paul Davies <paulmdavies@gmail.com>
2026-01-27 07:00:30 +0000

    First cut of blog

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from blog import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('posts/', views.posts, name='posts'),
+    path('posts/<slug:slug>', views.post, name='post'),
 ]
