b0540339b37ff525118274b82d241427f9c3356f
Paul Davies <paulmdavies@gmail.com>
2026-02-26 17:26:07 +0000

    Add favourite functionality

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from reviews.views import ReviewView, ThanksView, ReviewsView, ReviewDetailView
+from reviews.views import ReviewView, ThanksView, ReviewsView, ReviewDetailView, ReviewFavouriteView
 
 urlpatterns = [
     path('', ReviewView.as_view()),
     path('thanks', ThanksView.as_view()),
     path('reviews', ReviewsView.as_view()),
-    path('review/<int:pk>', ReviewDetailView.as_view(), name='review_detail')
+    path('review/<int:pk>', ReviewDetailView.as_view(), name='review_detail'),
+    path('reviews/favourite', ReviewFavouriteView.as_view())
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,5 @@
-from django.views.generic.base import TemplateView
+from django.http.response import HttpResponseRedirect
+from django.views.generic.base import TemplateView, View
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import FormView, CreateView
 from django.views.generic.list import ListView
@@ -27,3 +28,18 @@ class ReviewsView(ListView):
 
 class ReviewDetailView(DetailView):
     model = Review
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+
+        context_data['is_favourite'] = self.object.id == int(self.request.session.get('favourite_review'))
+
+        return context_data
+
+
+class ReviewFavouriteView(View):
+    def post(self, request):
+        review_id = request.POST['review_id']
+        request.session['favourite_review'] = review_id
+
+        return HttpResponseRedirect(f'/review/{review_id}')
